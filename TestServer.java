780c34741638679f16a1dfa1454a328efbdb6079
centurianomy <rawatchirag333@gmail.com>
2026-05-05 00:21:13 +0530

    update

--- TestServer.java
+++ TestServer.java
@@ -8,8 +8,16 @@ public class TestServer{
             //other usable ports: 5000, 8080 
             /* ports between 0-1023 are reserved for OS tasks and 1024-65000 are for normal use*/
             System.out.println("Server has started on port 1234...");
-            Thread.sleep(10000); // to keep the server running for a while, otherwise it will start and end immediately. (100000 milliseconds = 100 seconds)
-            System.out.println("Server closed...");
+            // to keep the server running for a while, otherwise it will start and end immediately. (10000 milliseconds = 10 seconds)
+            for(int i=0; i<5; i++){
+                if(i==4){
+                    System.out.println("Server is closing...");
+                    break;
+                }
+                System.out.println("Server is alive.");
+                Thread.sleep(2000); // to print the message every 2 seconds.
+            }
+            System.out.println("Server closed.");
         }
 
         catch(Exception e){
